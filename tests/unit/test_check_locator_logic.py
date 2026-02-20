538dbaecbbb7f6c51b0bdfbbdf43efc793fbdb14
Prasad <healsakrprasad@gmail.com>
2026-02-20 21:54:56 +0530

    adding few addon for report generation

--- tests/unit/test_check_locator_logic.py
+++ tests/unit/test_check_locator_logic.py
@@ -8,36 +8,32 @@ from utils.logger import Logger
 log=Logger().get_logger(__name__)
 
 class Test_check_locator_logic():
-    
+
     @pytest.mark.unit
     def test_check_locator_logic_success(self,page):
-
         login_button = Locator(
         by=LocateBy.TEXT,
         value="password",
         )
         base_page=BasePage(page)
         base_page._locate_element(login_button)
-    
+
     @pytest.mark.unit
     def test_check_locator_logic_name_missing(self,page):
-        
         login_button = Locator(
         by=LocateBy.ROLE,
         value="password",
         )
         base_page=BasePage(page)
-
         with pytest.raises(ValueError):
             base_page._locate_element(login_button)
-    
+
     @pytest.mark.unit
-    def test_check_locator_logic_failed(self,page):    
+    def test_check_locator_logic_failed(self,page):
         with pytest.raises(AttributeError):
             login_button = Locator(
             by=LocateBy.Test,
             value="password",
             )
-
             base_page=BasePage(page)
             base_page._locate_element(login_button)
